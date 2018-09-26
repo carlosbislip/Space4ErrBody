e35933c52b64c3c6472537d4e064d1b2a257f9c2
carlosbislip <carlosbislip@gmail.com>
2018-09-26 15:15:29 +0200

    Added extraction of target coordinates from body map. Coordinates will be used in guidance function.

--- Space4ErrBody_testing/Space4ErrBody_Executables_and_Headers_testing/updateGuidance_val.h
+++ Space4ErrBody_testing/Space4ErrBody_Executables_and_Headers_testing/updateGuidance_val.h
@@ -14,6 +14,7 @@ namespace aerodynamics
 class ValidationAerodynamicGuidance: public AerodynamicGuidance
 {
 public:
+
     ValidationAerodynamicGuidance(
             const tudat::simulation_setup::NamedBodyMap& bodyMap,
             const std::string vehicleName)
@@ -21,6 +22,9 @@ public:
     {
         FlightConditions_ = boost::dynamic_pointer_cast< AtmosphericFlightConditions >(
                     bodyMap.at( vehicleName )->getFlightConditions( ) );
+        lat_f_rad_ = bodyMap.at( vehicleName )->getTargetLat( ) ;
+        lon_f_rad_ = bodyMap.at( vehicleName )->getTargetLon( ) ;
+//     const double    lon_f_rad_ = lon_f_rad;
     }
 
     void updateGuidance(const double currentTime);
@@ -28,6 +32,10 @@ public:
 private:
 
     boost::shared_ptr< AtmosphericFlightConditions > FlightConditions_;
+    double lat_f_rad_;
+    double lon_f_rad_;
+
+
 };
 
 } // namespace aerodynamics

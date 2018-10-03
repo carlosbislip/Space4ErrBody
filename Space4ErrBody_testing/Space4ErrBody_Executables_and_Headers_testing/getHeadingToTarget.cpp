fdf28ce1ed612cf08019ebd0d94611bbbab795fa
carlosbislip <carlosbislip@gmail.com>
2018-10-03 17:33:20 +0200

    Created function to calculate heading to a target.

---
+++ Space4ErrBody_testing/Space4ErrBody_Executables_and_Headers_testing/getHeadingToTarget.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+double getHeadingToTarget ( const double &lat_i,
+    const double &lon_i,
+    const double &lat_f,
+    const double &lon_f)
+{
+    const double heading_to_target = std::atan2( std::sin( lon_f - lon_i ) * std::cos( lat_f ) , std::cos( lat_i ) * std::sin( lat_f ) - std::sin( lat_i ) * std::cos( lat_f ) * std::cos( lon_f - lon_i ) );
+
+    return heading_to_target;
+    
+}

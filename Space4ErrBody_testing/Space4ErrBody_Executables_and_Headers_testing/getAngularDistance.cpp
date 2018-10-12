86c061a2212203a0938e3d98e66967631d80a420
carlosbislip <carlosbislip@gmail.com>
2018-10-12 22:15:27 +0200

    Cleaned up spacing.

--- Space4ErrBody_testing/Space4ErrBody_Executables_and_Headers_testing/getAngularDistance.cpp
+++ Space4ErrBody_testing/Space4ErrBody_Executables_and_Headers_testing/getAngularDistance.cpp
@@ -4,15 +4,13 @@
 #include <fstream>
 #include <cmath>
 
-double getAngularDistance ( const double &lat_i,
+double getAngularDistance (
+	const double &lat_i,
     const double &lon_i,
     const double &lat_f,
     const double &lon_f)
 {
     const double angular_distance = std::acos( std::sin(lat_i) * std::sin(lat_f) + std::cos(lat_i) * std::cos(lat_f) * std::cos(lon_f - lon_i) );
 
-
-
     return angular_distance;
-    
 }

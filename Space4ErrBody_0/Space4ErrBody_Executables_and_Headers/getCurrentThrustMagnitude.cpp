001d51ac304148bd08b55ab7f9b741b9f9e2b401
carlosbislip <carlosbislip@gmail.com>
2018-11-01 09:45:48 +0100

    Function to get thrust magnitude.

---
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/getCurrentThrustMagnitude.cpp
@@ -0,0 +1,11 @@
+#include "updateGuidance.h"
+
+namespace bislip {
+
+double MyThrustGuidance::getCurrentThrustMagnitude( )
+{
+
+    return currentThrustMagnitude_;
+}
+} // namespace aerodynamics
+//} // namespace tudat

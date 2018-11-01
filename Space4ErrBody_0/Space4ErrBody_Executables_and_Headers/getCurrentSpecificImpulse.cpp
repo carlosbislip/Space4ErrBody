c33a0f69a30c11f72ac15098540177fac0e8a4c8
carlosbislip <carlosbislip@gmail.com>
2018-11-01 11:06:32 +0100

    Function for current specific impulse.

---
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/getCurrentSpecificImpulse.cpp
@@ -0,0 +1,11 @@
+#include "updateGuidance.h"
+
+namespace bislip {
+
+double MyThrustGuidance::getCurrentSpecificImpulse( )
+{
+
+    return currentSpecificImpulse_;
+}
+} // namespace aerodynamics
+//} // namespace tudat

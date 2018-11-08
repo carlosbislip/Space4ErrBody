eaf92b8675fb94d69510a0871ac3beaaafe3da67
carlosbislip <carlosbislip@gmail.com>
2018-11-08 12:51:42 +0100

    Added bislip namespace

--- Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/StopOrNot.h
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/StopOrNot.h
@@ -3,12 +3,14 @@
 
 #include <Tudat/SimulationSetup/tudatSimulationHeader.h>
 
+namespace bislip {
+
 bool StopOrNot ( const tudat::simulation_setup::NamedBodyMap& bodyMap,
                  const std::string &vehicleName,
                  const std::vector< double > term_cond,
                  const std::vector< double > additional_data );
 
-
+}
 
 #endif // CUSTOMTERMINATIONFUNCTION_VAL_H
 

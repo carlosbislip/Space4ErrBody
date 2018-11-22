cf3acf8d4aff9fa65b13fcc8401b516215189b63
carlosbislip <carlosbislip@gmail.com>
2018-11-22 14:11:30 +0100

    Added various variables.

--- Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/StopOrNot.h
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/StopOrNot.h
@@ -2,13 +2,21 @@
 #define CUSTOMTERMINATIONFUNCTION_VAL_H
 
 #include <Tudat/SimulationSetup/tudatSimulationHeader.h>
+#include <Tudat/Astrodynamics/BasicAstrodynamics/unitConversions.h>
+#include <Tudat/Astrodynamics/Aerodynamics/flightConditions.h>
+#include <Tudat/Astrodynamics/Aerodynamics/aerodynamics.h>
+#include <Tudat/Astrodynamics/Aerodynamics/aerodynamicGuidance.h>
 
 namespace bislip {
 
 bool StopOrNot ( const tudat::simulation_setup::NamedBodyMap& bodyMap,
                  const std::string &vehicleName,
-                 const std::vector< double > term_cond,
-                 const std::vector< double > additional_data );
+                 const std::vector< double > &parameterBounds,
+                 const std::vector< double > &terminationConditionsValues_,
+                 const std::vector< double > &additional_data,
+                 const double &E_max,
+                 const std::shared_ptr< tudat::interpolators::OneDimensionalInterpolator< double, double > > &interpolator_throttle,
+                 const std::shared_ptr< tudat::interpolators::OneDimensionalInterpolator< double, double > > &interpolator_eps_T_deg);
 
 }
 

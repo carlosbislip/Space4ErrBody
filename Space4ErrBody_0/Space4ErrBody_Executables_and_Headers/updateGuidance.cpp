262c7296a216be474640117784c08a2ecf539bb0
carlosbislip <carlosbislip@gmail.com>
2018-12-09 20:16:40 +0100

    Cleaned up and streamlined evaluation of guidance interpolators.

--- Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/updateGuidance.cpp
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/updateGuidance.cpp
@@ -1,13 +1,5 @@
-//#include "Space4ErrBody.h"
 #include "updateGuidance.h"
-//#include "multi_dimensional_root_finding.hpp"
-//#include "getStuff.h"
-//#include "search_Coefficients.h"
 #include "bislipVariables.h"
-
-//#include <boost/numeric/ublas/vector.hpp>
-//#include <boost/numeric/ublas/io.hpp>
-//#include <Eigen/Dense>
 
 #include <Tudat/Mathematics/BasicMathematics/mathematicalConstants.h>
 #include <Tudat/Astrodynamics/BasicAstrodynamics/unitConversions.h>
@@ -21,7 +13,7 @@ namespace bislip {
 
 void MyGuidance::updateGuidance( const double currentTime )
 {
-    std::cout << "Starting Aerodynamic guidance for this evaluation" << std::endl;
+    //std::cout << "Starting Aerodynamic guidance for this evaluation" << std::endl;
 
     if( currentTime == currentTime )
     {
@@ -32,16 +24,11 @@ void MyGuidance::updateGuidance( const double currentTime )
                         bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
         }
 
-
-        // E_hat = getE_hat();
-        //std::cout << "E_hat: " << E_hat << std::endl;
-
         //       if( coefficientInterface_ == nullptr )
         //       {
         //           coefficientInterface_ = std::dynamic_pointer_cast< tudat::aerodynamics::AerodynamicCoefficientInterface >(
         //                       bodyMap_.at( vehicleName_ )->getAerodynamicCoefficientInterface( ) );
         //       }
-
 
         //! Set of parameters that I am yet to figure out how to pass/extract them around.
         const double b_ref = 13;
@@ -55,8 +42,6 @@ void MyGuidance::updateGuidance( const double currentTime )
         const double J2 = 1082.626523e-6;
         const double J3 = 2.532153e-7;
         const double J4 = 1.6109876e-7;
-        //std::shared_ptr< tudat::aerodynamics::AtmosphericFlightConditions > FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
-        //                bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
 
         //! Extract various parameters form current flight conditions
         double current_height = FlightConditions_->getCurrentAltitude( );
@@ -84,161 +69,30 @@ void MyGuidance::updateGuidance( const double currentTime )
         const double r_norm = r.norm( );
         const double V_norm = current_V;
 
-        double AoA = bislip::variables::computeThrottleSetting(
-                    bodyMap_.at( vehicleName_ )->getAoAInterpolator(),
+        std::shared_ptr< tudat::system_models::VehicleSystems > vehicleSystems = bodyMap_.at( vehicleName_ )->getVehicleSystems();
+
+        const double AoA = bislip::variables::evaluateGuidanceInterpolator (
+                    FlightConditions_->getAerodynamicAngleCalculator( )->getAerodynamicAngle( tudat::reference_frames::flight_path_angle ),
+                    "Angle of Attack",
+                    vehicleSystems,
                     FlightConditions_->getCurrentAltitude(),
                     FlightConditions_->getCurrentAirspeed(),
-                    bodyMap_.at( vehicleName_ )->getE_max( ) );
+                    vehicleSystems->getE_max() );
 
-        if ( AoA < parameterBounds_[ 2 ] )
-        {
-            AoA = parameterBounds_[ 2 ];
-        }
-        if ( AoA > parameterBounds_[ 3 ] )
-        {
-            AoA = parameterBounds_[ 3 ];
-        }
+        const double sigma = bislip::variables::evaluateGuidanceInterpolator (
+                    FlightConditions_->getAerodynamicAngleCalculator( )->getAerodynamicAngle( tudat::reference_frames::flight_path_angle ),
+                    "Bank Angle",
+                    vehicleSystems,
+                    FlightConditions_->getCurrentAltitude(),
+                    FlightConditions_->getCurrentAirspeed(),
+                    vehicleSystems->getE_max() );
 
         currentAngleOfAttack_ = tudat::unit_conversions::convertDegreesToRadians( AoA );
-        currentBankAngle_ = 0;
+        currentBankAngle_ = tudat::unit_conversions::convertDegreesToRadians( sigma );
         currentAngleOfSideslip_ = 0;
 
-        /*        //currentSpecificImpulse_ = Isp_;
-
-        //! Simplified expressions becuase thrust azimuth is known to be zero. I.e. phi_T = 0
-       // currentbodyFixedThrustDirection_( 0 ) = std::cos( tudat::unit_conversions::convertDegreesToRadians( eps_T ) );
-       // currentbodyFixedThrustDirection_( 1 ) = 0.0;
-       // currentbodyFixedThrustDirection_( 2 ) = std::sin( tudat::unit_conversions::convertDegreesToRadians( eps_T ) );
-
-        //currentThrustMagnitude_ = throttle * maxThrust_;
-
-        //std::cout << "currentMass: " << currentMass << std::endl;
-        //std::cout << "currentbodyFixedThrustDirection_: " << currentbodyFixedThrustDirection_ << std::endl;
-        //std::cout << "currentThrustMagnitude_: " << currentThrustMagnitude_ << std::endl;
-        //std::cout << "currentEngineStatus_: " << currentEngineStatus_ << std::endl;
-        //std::cout << "currentSpecificImpulse_: " << currentSpecificImpulse_ << std::endl;
-        //std::cout << "----------------------------"<< std::endl;
-
-        // double alpha_deg = interpolator_alpha_deg_->interpolate( E );
-        // Define input to aerodynamic coefficients: take care of order of input (this depends on how the coefficients are created)!
-        //Eigen::VectorXd coefficient_input ( 2 );
-        // Define input to aerodynamic coefficients: take care of order of input (this depends on how the coefficients are created)!
-        std::vector< double > coefficient_input;
-        coefficient_input.push_back( currentAngleOfAttack_ );
-        coefficient_input.push_back( current_M );
-
-        //       Eigen::Vector6d newCoefficients = MyGuidance::getCoefficients( coefficient_input );
-
-        std::cout << std::fixed << std::setprecision(10) <<
-                     std::setw(15) << "  E_hat:  " <<
-                     std::setw(16) << E_hat <<
-                     std::setw(15) << "  alpha_deg:  " <<
-                     std::setw(16) << interpolator_alpha_deg_->interpolate( E_hat ) <<
-                     //std::setw(15) << "  eps_T_deg: " <<
-                     //std::setw(16) << eps_T_deg <<
-                     //std::setw(15) << "  eps_T_deg: " <<
-                     //std::setw(16) << eps_T_deg <<
-                     //std::setw(8) << "new_V: " <<
-                     //std::setw(16) << new_V(0)  << "  " <<new_V(1)<< "  " <<new_V(2) <<
-                     //std::setw(15) << "  throttle:   "<<
-                     //std::setw(16) << throttle_ <<
-                     std::setw(10) << "  CD:    " <<
-                     std::setw(16) << newCoefficients[0] <<
-                     std::setw(10) << "  CL:    " <<
-                     std::setw(16) << newCoefficients[2] <<
-                     std::setw(10) << "  Cm:    " <<
-                     std::setw(16) << newCoefficients[4] <<
-                     std::setw(10) << "  Vx:    " <<
-                     std::setw(16) << V[0] <<
-                     std::setw(10) << "  Vy:    " <<
-                     std::setw(16) << V[1] <<
-                     std::setw(10) << "  Vz:    " <<
-                     std::setw(16) << V[2] << std::endl;
-
-*/
     }
 
-}
-/*
-Eigen::Vector3d  MyGuidance::getCurrentBodyFixedThrustDirection( )
-{
-
-    if( FlightConditions_ == nullptr )
-    {
-        FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
-                    bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
-    }
-
-    double eps_T = bislip::variables::computeThrustElevationAngle(
-                bodyMap_.at( vehicleName_ )->getThrustElevationAngleInterpolator(),
-                FlightConditions_->getCurrentAltitude(),
-                FlightConditions_->getCurrentAirspeed(),
-                bodyMap_.at( vehicleName_ )->getE_max( ) );
-
-    if ( eps_T < parameterBounds_[ 4 ] )
-    {
-        eps_T = parameterBounds_[ 4 ];
-    }
-    if ( eps_T > parameterBounds_[ 5 ] )
-    {
-        eps_T = parameterBounds_[ 5 ];
-    }
-
-    //! Simplified expressions becuase thrust azimuth is known to be zero. I.e. phi_T = 0
-    currentbodyFixedThrustDirection_( 0 ) = std::cos( tudat::unit_conversions::convertDegreesToRadians( eps_T ) );
-    currentbodyFixedThrustDirection_( 1 ) = 0.0;
-    currentbodyFixedThrustDirection_( 2 ) = std::sin( tudat::unit_conversions::convertDegreesToRadians( eps_T ) );
-
-    return currentbodyFixedThrustDirection_;
-}
-
-double  MyGuidance::getCurrentThrustMagnitude( )
-{
-    if( FlightConditions_ == nullptr )
-    {
-        FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
-                    bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
-    }
-
-    double throttle = bislip::variables::computeThrottleSetting(
-                bodyMap_.at( vehicleName_ )->getThrottleInterpolator(),
-                FlightConditions_->getCurrentAltitude(),
-                FlightConditions_->getCurrentAirspeed(),
-                bodyMap_.at( vehicleName_ )->getE_max( ) );
-
-    if ( throttle < 0.0 )
-    {
-        throttle = 0.0;
-    }
-    if ( throttle > 1.0 )
-    {
-        throttle = 1.0;
-    }
-
-  //  currentThrustMagnitude_ = throttle * maxThrust_;
-
-    return throttle * maxThrust_;
-}
-double  MyGuidance::getCurrentSpecificImpulse( )
-{
-    //currentSpecificImpulse_ = Isp_;
-
-    return Isp_;
-}
-bool  MyGuidance::getCurrentEngineStatus( )
-{
-    if( FlightConditions_ == nullptr )
-    {
-        FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
-                    bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
-    }
-
-    //currentEngineStatus_ = bislip::variables::determineEngineStatus(  bodyMap_.at( vehicleName_ )->getBodyMass(), bodyMap_.at( vehicleName_ )->getLandingMass() );
-
-    return bislip::variables::determineEngineStatus(  bodyMap_.at( vehicleName_ )->getBodyMass(), bodyMap_.at( vehicleName_ )->getLandingMass() );
-}*/
-
-
-
+} // MyGuidance
 } // namespace bislip
 

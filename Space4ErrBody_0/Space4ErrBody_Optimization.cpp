1750ddf7fa02862a2baef1e0f1e1382f158a6442
carlosbislip <carlosbislip@gmail.com>
2018-12-09 19:42:39 +0100

    Expanded code to include descent parameters.

--- Space4ErrBody_0/Space4ErrBody_Optimization.cpp
+++ Space4ErrBody_0/Space4ErrBody_Optimization.cpp
@@ -9,6 +9,7 @@
 #include "Space4ErrBody_Executables_and_Headers/applicationOutput_pagmo.h"
 #include "Space4ErrBody_Executables_and_Headers/saveOptimizationResults.h"
 #include "Space4ErrBody_Executables_and_Headers/getStuff.h"
+#include "Space4ErrBody_Executables_and_Headers/bislipVariables.h"
 
 //#include "Space4ErrBody_Executables_and_Headers_testing/getConditions.h"
 //#include "Space4ErrBody_Executables_and_Headers_testing/getOptimizationSettings.h"
@@ -34,6 +35,7 @@ using namespace tudat;
 using namespace tudat_applications;
 using namespace pagmo;
 using namespace bislip;
+using namespace bislip::variables;
 using namespace boost;
 using namespace tudat_pagmo_applications;
 
@@ -49,21 +51,23 @@ int main()
 
     //! Get all inputs.
     const std::vector< std::string > primary                   = getDataString ( "primary.txt" );
-    const std::vector< std::string > parameterList             = getDataString ( primary[ 0 ] );
-    const std::vector< double > parameterBounds                = getDataNumeri ( primary[ 1 ] );
-    const std::vector< std::string > vehicleParameterList      = getDataString ( primary[ 2 ] );
-    const std::vector< double > vehicleParameterValues         = getDataNumeri ( primary[ 3 ] );
-    const std::vector< std::string > aeroCoeffFileList         = getDataString ( primary[ 4 ] );
-    const std::vector< std::string > optimization_settingsList = getDataString ( primary[ 5 ] );
-    const std::vector< double > optimization_settingsValues    = getDataNumeri ( primary[ 6 ] );
-    const std::vector< std::string > simulation_settingsList   = getDataString ( primary[ 7 ] );
-    const std::vector< double > simulation_settingsValues      = getDataNumeri ( primary[ 8 ] );
-    const std::vector< std::string > output_settingsList       = getDataString ( primary[ 9 ] );
-    const std::vector< double > output_settingsValues          = getDataNumeri ( primary[ 10 ] );
-    const std::vector< std::string > initialConditionsList     = getDataString ( primary[ 11 ] );
-    const std::vector< double > initialConditionsValues        = getDataNumeri ( primary[ 12 ] );
-    const std::vector< std::string > terminationConditionsList = getDataString ( primary[ 13 ] );
-    const std::vector< double > terminationConditionsValues    = getDataNumeri ( primary[ 14 ] );
+    const std::vector< std::string > parameterList_Ascent      = getDataString ( primary[ 0 ] );
+    const std::vector< double > parameterBounds_Ascent         = getDataNumeri ( primary[ 1 ] );
+    const std::vector< std::string > parameterList_Descent     = getDataString ( primary[ 2 ] );
+    const std::vector< double > parameterBounds_Descent        = getDataNumeri ( primary[ 3 ] );
+    const std::vector< std::string > vehicleParameterList      = getDataString ( primary[ 4 ] );
+    const std::vector< double > vehicleParameterValues         = getDataNumeri ( primary[ 5 ] );
+    const std::vector< std::string > aeroCoeffFileList         = getDataString ( primary[ 6 ] );
+    const std::vector< std::string > optimization_settingsList = getDataString ( primary[ 7 ] );
+    const std::vector< double > optimization_settingsValues    = getDataNumeri ( primary[ 8 ] );
+    const std::vector< std::string > simulation_settingsList   = getDataString ( primary[ 9 ] );
+    const std::vector< double > simulation_settingsValues      = getDataNumeri ( primary[ 10 ] );
+    const std::vector< std::string > output_settingsList       = getDataString ( primary[ 11 ] );
+    const std::vector< double > output_settingsValues          = getDataNumeri ( primary[ 12 ] );
+    const std::vector< std::string > initialConditionsList     = getDataString ( primary[ 13 ] );
+    const std::vector< double > initialConditionsValues        = getDataNumeri ( primary[ 14 ] );
+    const std::vector< std::string > terminationConditionsList = getDataString ( primary[ 15 ] );
+    const std::vector< double > terminationConditionsValues    = getDataNumeri ( primary[ 16 ] );
     const std::string problem_name                             = primary.rbegin()[ 1 ];
     const std::string vehicle_name                             = primary.rbegin()[ 0 ];
 
@@ -83,12 +87,18 @@ int main()
     //! previously created partial suffix, and the run's time stamp.
     const std::string outputSubFolder = "OUTPUT_" + this_run_settings + "_" + play_time + "/";
 
-    //! Determine number of parameters to vary based of size of parameterBounds vector.
-    const int nodes = simulation_settingsValues[ 3 ];
-    const int N = parameterList.size() * nodes - 1;
+    //! Determine number of Ascent parameters to vary based of size of parameterBounds_Ascent vector.
+    const int nodes_Ascent = simulation_settingsValues[ 3 ];
+    const int N = ( parameterList_Ascent.size() - 3 ) * nodes_Ascent - 1;
+    const int M = parameterBounds_Ascent.size();
+
+    //! Determine number of Descent parameters to vary based of size of parameterBounds_Descent vector.
+    const int nodes_Descent = simulation_settingsValues[ 4 ];
+    const int NN = ( parameterList_Descent.size() - 1 ) * nodes_Descent - 1;
+    const int MM = parameterBounds_Descent.size();
 
     //! Create vector containing decision vector bounds.
-    std::vector< std::vector< double > > bounds( 2, std::vector< double >( N, 0.0 ) );
+    std::vector< std::vector< double > > bounds( 2, std::vector< double >( N + 3 + NN + 1, 0.0 ) );
     //!---------------------------------------   ^ for lb/up  (rows)       ^ for # of parameters
 
     //! Loop to build the bounds matrix.
@@ -96,18 +106,68 @@ int main()
 
     for( int i = 0; i < N; i++ )
     {
-        if( i <  ( nodes -1 ) )
+        if( i < ( nodes_Ascent -1 ) )
         {
             p = 0;
         }
-        else if( ( i - ( nodes - 1 ) ) % nodes == 0 )
+        else if( ( i - ( nodes_Ascent - 1 ) ) % nodes_Ascent == 0 )
         {
             p += 2;
         }
 
-        bounds[ 0 ][ i ] = parameterBounds[ p ];
-        bounds[ 1 ][ i ] = parameterBounds[ p + 1 ];
+        bounds[ 0 ][ i ] = parameterBounds_Ascent[ p ];
+        bounds[ 1 ][ i ] = parameterBounds_Ascent[ p + 1 ];
+        std::cout << "bounds[ 0 ][ " << i << " ] = " << parameterBounds_Ascent[ p ] << std::endl;
+        std::cout << "bounds[ 1 ][ " << i << " ] = " << parameterBounds_Ascent[ p + 1 ] << std::endl;
+
+
     }
+
+    bounds[ 0 ][ N ] = parameterBounds_Ascent[ M - 6 ];
+    bounds[ 1 ][ N ] = parameterBounds_Ascent[ M - 5 ];
+
+    bounds[ 0 ][ N + 1 ] = parameterBounds_Ascent[ M - 4 ];
+    bounds[ 1 ][ N + 1 ] = parameterBounds_Ascent[ M - 3 ];
+    bounds[ 0 ][ N + 2 ] = parameterBounds_Ascent[ M - 2 ];
+    bounds[ 1 ][ N + 2 ] = parameterBounds_Ascent[ M - 1 ];
+
+    std::cout << "bounds[ 0 ][ " << N << " ] = " << parameterBounds_Ascent[ M - 6 ] << std::endl;
+    std::cout << "bounds[ 1 ][ " << N << " ] = " << parameterBounds_Ascent[ M - 5 ] << std::endl;
+    std::cout << "bounds[ 0 ][ " << N +1<< " ] = " << parameterBounds_Ascent[ M - 4 ] << std::endl;
+    std::cout << "bounds[ 1 ][ " << N +1<< " ] = " << parameterBounds_Ascent[ M - 3 ] << std::endl;
+    std::cout << "bounds[ 0 ][ " << N +2<< " ] = " << parameterBounds_Ascent[ M - 2 ] << std::endl;
+    std::cout << "bounds[ 1 ][ " << N +2<< " ] = " << parameterBounds_Ascent[ M - 1 ] << std::endl;
+
+
+    for( int i = 0; i < NN ; i++ )
+    {
+        if( i < ( nodes_Descent -1 ) )
+        {
+            p = 0;
+        }
+        else if( ( i - ( nodes_Descent - 1 ) ) % nodes_Descent == 0 )
+        {
+            p += 2;
+        }
+
+        bounds[ 0 ][ ( N + 3 ) + i ] = parameterBounds_Descent[ p ];
+        bounds[ 1 ][ ( N + 3 ) + i ] = parameterBounds_Descent[ p + 1 ];
+        std::cout << "bounds[ 0 ][ " << ( N + 3 ) + i << " ] = " << parameterBounds_Descent[ p ] << std::endl;
+        std::cout << "bounds[ 1 ][ " << ( N + 3 ) + i << " ] = " << parameterBounds_Descent[ p + 1 ] << std::endl;
+    }
+
+    bounds[ 0 ][ N + NN + 3 ] = parameterBounds_Descent[ MM - 2 ];
+    bounds[ 1 ][ N + NN + 3 ] = parameterBounds_Descent[ MM - 1 ];
+    std::cout << "bounds[ 0 ][ " << ( N + 3 ) + NN << " ] = " << parameterBounds_Descent[ MM - 2 ] << std::endl;
+    std::cout << "bounds[ 1 ][ " << ( N + 3 ) + NN << " ] = " << parameterBounds_Descent[ MM - 1 ] << std::endl;
+
+    std::cout << "N = " << N << std::endl;
+    std::cout << "M = " << M << std::endl;
+    std::cout << "NN: " << NN << std::endl;
+    std::cout << "MM: " << MM << std::endl;
+    //std::cout << "parameterBounds_Descent[ MM - 2 ]: " << parameterBounds_Descent[ MM - 2 ] << std::endl;
+    //std::cout << "parameterBounds_Descent[ MM - 1 ]: " << parameterBounds_Descent[ MM - 1 ] << std::endl;
+    //std::cout << "N + NN + 3 + 1: " << N + NN + 3 + 1 << std::endl;
 
     //! Load spice kernels
     tudat::spice_interface::loadStandardSpiceKernels( );
@@ -122,7 +182,10 @@ int main()
     problem prob{ Space4ErrBody( bounds,
                                  problem_name,
                                  vehicle_name,
-                                 parameterList,
+                                 parameterList_Ascent,
+                                 parameterBounds_Ascent,
+                                 parameterList_Descent,
+                                 parameterBounds_Descent,
                                  vehicleParameterValues,
                                  aeroCoeffFileList,
                                  simulation_settingsValues,
@@ -187,7 +250,7 @@ int main()
     //! Calculate initial heading angle: https://www.movable-type.co.uk/scripts/latlong.html
     //const double chi_i_rad_calc = getHeadingToTarget( lat_i_rad , lon_i_rad , lat_f_rad , lon_f_rad );
     //std::atan2( std::sin( lon_f_rad - lon_i_rad ) * std::cos( lat_f_rad ) , std::cos( lat_i_rad ) * std::sin( lat_f_rad ) - std::sin( lat_i_rad ) * std::cos( lat_f_rad ) * std::cos( lon_f_rad - lon_i_rad ) );
-    double chi_i_deg_calc = unit_conversions::convertRadiansToDegrees( getHeadingToTarget( lat_i_rad , lon_i_rad , lat_f_rad , lon_f_rad ) );
+    double chi_i_deg_calc = unit_conversions::convertRadiansToDegrees( bislip::variables::computeHeadingToTarget( lat_i_rad , lon_i_rad , lat_f_rad , lon_f_rad ) );
     //! If heading angle is negative, this may help visualize it.
     if (chi_i_deg_calc < 0)
     {
@@ -197,7 +260,7 @@ int main()
     //! Calculate ground distance to cover using Haversine formula and Sperical Law of Cosines: https://www.movable-type.co.uk/scripts/latlong.html
     const double a = std::sin( (lat_f_rad - lat_i_rad) / 2) * std::sin( (lat_f_rad - lat_i_rad) / 2) + std::cos( lat_i_rad ) * std::cos( lon_i_rad ) * std::sin( (lon_f_rad - lon_i_rad) / 2) * std::sin( (lon_f_rad - lon_i_rad) / 2);
     const double d_angular = 2 * std::atan2( std::sqrt(a) , std::sqrt(1 - a) );
-    const double d_spherical_law_cosines = unit_conversions::convertRadiansToDegrees( getAngularDistance( lat_i_rad , lon_i_rad , lat_f_rad , lon_f_rad ) );
+    const double d_spherical_law_cosines = unit_conversions::convertRadiansToDegrees( bislip::variables::computeAngularDistance( lat_i_rad , lon_i_rad , lat_f_rad , lon_f_rad ) );
     //std::acos( std::sin(lat_i_rad) * std::sin(lat_f_rad) + std::cos(lat_i_rad) * std::cos(lat_f_rad) * std::cos(lon_f_rad-lon_i_rad) ) * spice_interface::getAverageRadius( "Earth" );
 
     //! Print to Terminal screen.
@@ -205,13 +268,22 @@ int main()
     std::cout << problem_name << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
-    std::cout << "Parameters to vary" << std::endl;
+    std::cout << "Parameters to vary during Ascent" << std::endl;
     p=0;
-    for( int i = 0; i < int( parameterList.size() ); i++)
+    for( int i = 0; i < int( parameterList_Ascent.size() ) ; i++)
     {
-        std::cout << boost::format("%-25s %-2s %-8.1f %-2s %-8.1f %-2s\n") % parameterList[i] % "[" % parameterBounds[p] % "," % parameterBounds[p+1] % "]";
-    p+=2;
+        std::cout << boost::format("%-25s %-2s %-8.1f %-2s %-8.1f %-2s\n") % parameterList_Ascent[i] % "[" % parameterBounds_Ascent[p] % "," % parameterBounds_Ascent[p+1] % "]";
+        p+=2;
     }
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::cout << "Parameters to vary during Descent" << std::endl;
+    p=0;
+    for( int i = 0; i < int( parameterList_Descent.size() ) ; i++)
+    {
+        std::cout << boost::format("%-25s %-2s %-8.1f %-2s %-8.1f %-2s\n") % parameterList_Descent[i] % "[" % parameterBounds_Descent[p] % "," % parameterBounds_Descent[p+1] % "]";
+        p+=2;
+    }
+
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "Vehicle Parameters" << std::endl;
     std::cout << std::setw(30) << vehicleParameterList[0] << "      " <<  vehicleParameterValues[0] << std::endl;
@@ -223,6 +295,7 @@ int main()
     std::cout << std::setw(30) << vehicleParameterList[12] << "      " <<  vehicleParameterValues[12] << std::endl;
     std::cout << std::setw(30) << vehicleParameterList[13] << "      " <<  vehicleParameterValues[13] << std::endl;
     std::cout << std::setw(30) << vehicleParameterList[14] << "      " <<  vehicleParameterValues[14] << std::endl;
+    std::cout << std::setw(30) << vehicleParameterList[15] << "      " <<  vehicleParameterValues[15] << std::endl;
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "Aerodynamic Coefficients" << std::endl;
     std::cout << "Coefficient File: " << "      " <<  aeroCoeffFileList[0] << std::endl;
@@ -266,8 +339,8 @@ int main()
     std::cout << "--------------------------------------------------------" << std::endl;
     std::cout << "Output subfolder: " << std::endl;
     std::cout << "     '" << outputSubFolder <<"'" << std::endl;
-    std::cout << " DV  " << " Lower Boundary " << " Upper Boundary " << std::endl;
-    for( int i = 0; i < N; i++ )
+    std::cout << " DV   " << "  Lower Boundary " << "  Upper Boundary " << std::endl;
+    for( int i = 0; i < N + NN + 3 + 1; i++ )
     {
         std::cout << std::fixed << std::setprecision(10) <<
                      std::setw(6) << i <<
@@ -310,10 +383,10 @@ int main()
     //! Write original (unevolved) population to file
     if  ( output_settingsValues[ 0 ] == 1 )
     {
-     //   printPopulationToFile( isl_pert.get_population( ).get_x( ), this_run_settings_1,
-     //                          outputSubFolder, false );
-     //   printPopulationToFile( isl_pert.get_population( ).get_f( ), this_run_settings_1,
-     //                          outputSubFolder, true );
+        //   printPopulationToFile( isl_pert.get_population( ).get_x( ), this_run_settings_1,
+        //                          outputSubFolder, false );
+        //   printPopulationToFile( isl_pert.get_population( ).get_f( ), this_run_settings_1,
+        //                          outputSubFolder, true );
     }
 
     for( int i = 0; i < evolutions; i++ )

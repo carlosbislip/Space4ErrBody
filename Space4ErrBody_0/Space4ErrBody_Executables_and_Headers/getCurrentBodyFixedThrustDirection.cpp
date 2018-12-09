e5da17dab0c73bc2259654092175df3b8cd666bd
carlosbislip <carlosbislip@gmail.com>
2018-12-09 19:48:26 +0100

    Functions used for thrust guidance.

---
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/getCurrentBodyFixedThrustDirection.cpp
@@ -0,0 +1,16 @@
+#include "updateGuidance.h"
+
+
+namespace bislip {
+
+Eigen::Vector3d MyGuidance::getCurrentBodyFixedThrustDirection(  )
+{
+    if( FlightConditions_ == nullptr )
+    {
+        FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
+                    bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
+    }
+
+    return bislip::variables::computeBodyFixedThrustDirection( FlightConditions_, bodyMap_.at( vehicleName_ )->getVehicleSystems( ) );
+}
+} // namespace bislip
---
+++ Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/getCurrentThrustMagnitude.cpp
@@ -0,0 +1,16 @@
+#include "updateGuidance.h"
+
+
+namespace bislip {
+
+double MyGuidance::getCurrentThrustMagnitude(  )
+{
+    if( FlightConditions_ == nullptr )
+    {
+        FlightConditions_ = std::dynamic_pointer_cast< tudat::aerodynamics::AtmosphericFlightConditions >(
+                    bodyMap_.at( vehicleName_ )->getFlightConditions( ) );
+    }
+
+    return bislip::variables::computeThrustMagnitude( FlightConditions_, bodyMap_.at( vehicleName_ )->getVehicleSystems( ) );
+}
+} // namespace bislip

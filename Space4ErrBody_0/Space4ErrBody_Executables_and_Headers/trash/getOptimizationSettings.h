8d6a975205205cc2b958c172194435d4877b111f
carlosbislip <carlosbislip@gmail.com>
2019-02-01 08:28:12 -0500

    Changed folder name.

--- Space4ErrBody_0/Space4ErrBody_Executables_and_Headers/trash/getOptimizationSettings.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <ctime>
-#include <sstream>
-
-std::vector< double > getOptimizationSettings ( const std::string &filename );
-
-std::vector< double > getConditions ( const std::string &filename );
-
-std::string getCurrentDateTime( const bool useLocalTime = false );
